dff74b7d95964fb5719dbb897b1d4a79299a773d
Ankur Bhatt <ankurbhatt04@gmail.com>
2020-01-03 13:27:17 +0530

    functionality commented added

--- seed.groovy
+++ seed.groovy
@@ -1,3 +1,23 @@
-job(ApplicationName){
-    description("Simple freestyle job created from seed.groovy")
+multibranchPipelineJob(ApplicationName){
+    description("Multi-branch job for Application $ApplicationName")
+    
+    // branchSources {
+    //     git {
+            // Sets credentials for authentication with the remote repository.
+            // credentialsId(String credentialsId)
+            
+            // Specifies a unique ID for this branch source.
+            // id('123456789')
+            // Sets the Git remote repository URL.
+            
+            // remote(String remote)
+    //     }
+    // }
+    // triggers {
+    //       urlTrigger(String crontab = null) {
+    //           url(String url){
+    //               check(lastModified)  // Check the lastModified and trigger automatically this job
+    //           }
+    //       }
+    // } 
 }
