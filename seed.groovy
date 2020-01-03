6f1be18b330bb682135fdee805d5cb866570ad74
Ankur Bhatt <ankurbhatt04@gmail.com>
2020-01-03 15:34:05 +0530

    deploy and multi branch job

--- seed.groovy
+++ seed.groovy
@@ -1,23 +1,67 @@
-multibranchPipelineJob(ApplicationName){
-    description("Multi-branch job for Application $ApplicationName")
-    
-    // branchSources {
-    //     git {
-            // Sets credentials for authentication with the remote repository.
-            // credentialsId(String credentialsId)
-            
-            // Specifies a unique ID for this branch source.
-            // id('123456789')
-            // Sets the Git remote repository URL.
-            
-            // remote(String remote)
-    //     }
-    // }
-    // triggers {
-    //       urlTrigger(String crontab = null) {
-    //           url(String url){
-    //               check(lastModified)  // Check the lastModified and trigger automatically this job
-    //           }
-    //       }
-    // } 
+def createDeploymentJob(jobName, repoUrl) {
+    pipelineJob(jobName) {
+        definition {
+            cpsScm {
+                scm {
+                    git {
+                        remote {
+                            url(repoUrl)
+                        }
+                        branches('master')
+                        extensions {
+                            cleanBeforeCheckout()
+                        }
+                    }
+                }
+                scriptPath("Jenkinsfile")
+            }
+        }
+    }
 }
+
+def createTestJob(jobName, repoUrl) {
+    multibranchPipelineJob(jobName) {
+        branchSources {
+            git {
+                remote(repoUrl)
+                includes('*')
+            }
+        }
+        triggers {
+            cron("H/5 * * * *")
+        }
+    }
+}
+
+def buildPipelineJobs() {
+    def repo = "https://github.com/kcrane3576/"
+    def repoUrl = repo + jobName + ".git"
+    def deployName = jobName + "_deploy"
+    def testName = jobName + "_test"
+
+    createDeploymentJob(deployName, repoUrl)
+    createTestJob(testName, repoUrl)
+}
+
+buildPipelineJobs()
+
+
+// branchSources {
+//     git {
+        // Sets credentials for authentication with the remote repository.
+        // credentialsId(String credentialsId)
+        
+        // Specifies a unique ID for this branch source.
+        // id('123456789')
+        // Sets the Git remote repository URL.
+        
+        // remote(String remote) // defines the git repository where the Jenkinsfile is located
+//     }
+// }
+// triggers {
+//       urlTrigger(String crontab = null) {
+//           url(String url){
+//               check(lastModified)  // Check the lastModified and trigger automatically this job
+//           }
+//       }
+// } 
