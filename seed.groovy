b6bda2fa91d808f8d1f1a81b5280296bd3d819f4
Ankur Bhatt <ankurbhatt04@gmail.com>
2020-01-03 16:13:46 +0530

    branch added

--- seed.groovy
+++ seed.groovy
@@ -3,9 +3,9 @@ def createTestJob(ApplicationName, repoUrl) {
         branchSources {
             git {
                 id('12342343r434fe2')
-                credentialsId('d594a6aa-4545-45fe-acca-d21d4088c63')    
+                credentialsId('d594a6aa-4545-45fe-acca-d21d4088c63f')    
                 remote(repoUrl)
-                includes('*')
+                includes('jenkinsFileTest')
             }
         }
         triggers {
