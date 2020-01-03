4fc0c1589fdc5ca6a29ad13050554ec26d2c2194
Ankur Bhatt <ankurbhatt04@gmail.com>
2020-01-03 16:08:55 +0530

    limit added

--- seed.groovy
+++ seed.groovy
@@ -3,7 +3,7 @@ def createTestJob(ApplicationName, repoUrl) {
         branchSources {
             git {
                 id('12342343r434fe2')
-                credentialsId('d594a6aa-4545-45fe-acca-d21d4088c63f')    
+                credentialsId('d594a6aa-4545-45fe-acca-d21d4088c63')    
                 remote(repoUrl)
                 includes('*')
             }
@@ -11,6 +11,12 @@ def createTestJob(ApplicationName, repoUrl) {
         triggers {
             cron("H/5 * * * *")
         }
+        
+        orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(5)
+        }
+    }
     }
 }
 
